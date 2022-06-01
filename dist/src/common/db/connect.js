c11d084b0e21678adf8d43dc01f6120f54e12aca
Merlin-3D <axel.deffo@gmail.com>
2022-06-01 21:55:01 +0100

    save template 9

--- dist/src/common/db/connect.js
+++ dist/src/common/db/connect.js
@@ -7,17 +7,13 @@ const path_1 = __importDefault(require("path"));
 const isCompiled = path_1.default.extname(__filename).includes("js");
 exports.default = {
     type: "postgres",
-    host: "ec2-54-227-248-71.compute-1.amazonaws.com" || "localhost",
+    host: "ec2-54-227-248-71.compute-1.amazonaws.com",
     port: 5432,
-    username: "tmunfslmykqhoh" || "postgres",
-    password: "b942197d0bd443d5f5546de53179b0bbda07e24ccbd02e17c04344fcb17e93d4" ||
-        "root",
-    database: "de5a878l6dmqj8" || "stock_db",
-    synchronize: !process.env.DB_NO_SYNC,
-    logging: !process.env.DB_NO_LOGS,
-    autoReconnect: true,
-    reconnectTries: Number.MAX_VALUE,
-    reconnectInterval: 2000,
+    username: "tmunfslmykqhoh",
+    password: "b942197d0bd443d5f5546de53179b0bbda07e24ccbd02e17c04344fcb17e93d4",
+    database: "de5a878l6dmqj8",
+    synchronize: true,
+    logging: true,
     entities: [`database/entity/**/*.${isCompiled ? "js" : "ts"}`],
     migrations: [`database/migration/**/*.${isCompiled ? "js" : "ts"}`],
     cli: {
