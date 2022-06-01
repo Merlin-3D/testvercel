35d41e70ae9aca5a0ca04b33fa4f5b741161bcc2
Merlin-3D <axel.deffo@gmail.com>
2022-06-01 20:42:40 +0100

    save template 5

--- dist/index.js
+++ dist/index.js
@@ -6,14 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
-const user_route_1 = require("./src/common/user-route");
-const connect_1 = __importDefault(require("./src/common/db/connect"));
-const typeorm_1 = require("typeorm");
 const routes = [];
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
-(0, typeorm_1.createConnection)(connect_1.default);
+// createConnection(ORMConfig);
 app.use(express_1.default.json());
 // here we are adding middleware to allow cross-origin requests
 app.use((0, cors_1.default)());
@@ -31,13 +28,13 @@ app.use((0, cors_1.default)());
 // app.use(expressWinston.logger(loggerOptions));
 // here we are adding the UserRoutes to our array,
 // this is a simple route to make sure everything is working properly
-routes.push(new user_route_1.UsersRoutes(app));
+// routes.push(new UsersRoutes(app));
 app.get("/", (req, res) => {
     res.send("Express + TypeScript Server + Typeorm");
 });
-routes.forEach((route) => {
-    // debugLog(`Routes configured for ${route.getName()}`);
-});
+// routes.forEach((route: CommonRoutesConfig) => {
+//   // debugLog(`Routes configured for ${route.getName()}`);
+// });
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
--- index.ts
+++ index.ts
@@ -15,7 +15,7 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-createConnection(ORMConfig);
+// createConnection(ORMConfig);
 app.use(express.json());
 
 // here we are adding middleware to allow cross-origin requests
@@ -39,15 +39,15 @@ app.use(cors());
 // here we are adding the UserRoutes to our array,
 
 // this is a simple route to make sure everything is working properly
-routes.push(new UsersRoutes(app));
+// routes.push(new UsersRoutes(app));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server + Typeorm");
 });
 
-routes.forEach((route: CommonRoutesConfig) => {
-  // debugLog(`Routes configured for ${route.getName()}`);
-});
+// routes.forEach((route: CommonRoutesConfig) => {
+//   // debugLog(`Routes configured for ${route.getName()}`);
+// });
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
